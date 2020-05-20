c88b0f9f838bf99e00c5459454472bf12dab3823
Ethan <ashethank@gmail.com>
2020-05-19 23:16:36 -0500

    completed contestants.js

--- JavaScript/contestants.js
+++ JavaScript/contestants.js
@@ -1,5 +1,23 @@
 $(() => {
 
+  let score = 0;
+
+  sessionStorage.setItem('score', score)
+
+  let one = Math.ceil(Math.random() * 63);
+  let two = Math.ceil(Math.random() * 63);
+  let three = Math.ceil(Math.random() * 63);
+  let four = Math.ceil(Math.random() * 63);
+  let five = Math.ceil(Math.random() * 63);
+  let six = Math.ceil(Math.random() * 63);
+
+  sessionStorage.setItem('one', one);
+  sessionStorage.setItem('two', two);
+  sessionStorage.setItem('three', three);
+  sessionStorage.setItem('four', four);
+  sessionStorage.setItem('five', five);
+  sessionStorage.setItem('six', six);
+
 $('#name').text(sessionStorage.getItem('name'));
 $('#occupation').text(sessionStorage.getItem('occupation'));
 $('#city').text(sessionStorage.getItem('city'));
